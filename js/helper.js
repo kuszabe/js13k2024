8d5cc8405b15b33849e32a42c9264d6912a62dce
kuszabe <kuszabe@gmail.com>
2024-09-04 23:31:11 +0200

    created the SAT collison detection in a separate file with a canvas

--- js/helper.js
+++ js/helper.js
@@ -7,8 +7,27 @@ function Vec2(x = 0, y = 0) {
 }
 
 Vec2.prototype = {
-    sub: (vec) => { return { x: this.x - vec.x, y: this.y - vec.y } },
-    add: (vec) => { return { x: this.x + vec.x, y: this.y + vec.y } },
-    eq: (vec) => this.x == vec.x && this.y == vec.y,
-    distance: (vec) => Math.sqrt(Math.pow(this.x - vec.x, 2) + Math.pow(this.y - vec.y, 2))
+    sub(vec) {return new Vec2(this.x - vec.x, this.y - vec.y)},
+    add(vec) {return new Vec2(this.x + vec.x, this.y + vec.y)},
+    scale(input) {
+        if (input instanceof Vec2) {
+            return new Vec2(this.x * input.x, this.y * input.y)
+        } else if (typeof input == "number") {
+            return new Vec2(this.x * input, this.y * input)
+        }
+    },
+    abs() {return new Vec2(Math.sqrt(this.x ** 2), Math.sqrt(this.y ** 2))},
+    len() {
+        return Math.sqrt(this.x ** 2 + this.y ** 2)
+    },
+    normal() {
+        let magnitude = this.len()
+        return new Vec2(this.x / magnitude, this.y / magnitude)
+    },
+    dot(vec) {
+        return this.x * vec.x + this.y * vec.y
+    },
+    eq(vec) {return this.x == vec.x && this.y == vec.y},
+    distance(vec) {return Math.sqrt(Math.pow(this.x - vec.x, 2) + Math.pow(this.y - vec.y, 2))},
+    perp() {return new Vec2(this.y, -this.x)}
 }
